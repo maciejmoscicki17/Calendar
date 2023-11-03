8e823516f7c774bf8d6771554b247abdeccf3abc
maciejmoscicki17 <73691737+maciejmoscicki17@users.noreply.github.com>
2023-11-03 21:20:21 +0100

    global loader and loader service added

--- CalendarClient/src/app/app.module.ts
+++ CalendarClient/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component'
 import { SidePanelComponent } from './side-panel/side-panel.component'
 import { CalendarModule } from './calendar/calendar.module';
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component'
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoaderComponent } from './loader/loader.component'
 @NgModule({
-    declarations: [AppComponent, SidePanelComponent, HomeComponent, NotFoundComponent],
+    declarations: [AppComponent, SidePanelComponent, HomeComponent, NotFoundComponent, LoaderComponent],
     imports: [BrowserModule, AppRoutingModule, CalendarModule],
     providers: [],
     bootstrap: [AppComponent],
--- CalendarClient/src/app/home/home.component.ts
+++ CalendarClient/src/app/home/home.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { LoaderService } from '../services/loader.service'
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+    constructor(private loaderService: LoaderService) {}
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    onClick() {
+        this.loaderService.shootLoader(3000)
+    }
 }
